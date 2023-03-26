3cd690745a3e50cda330dd1f25ead5ffe47736d0
wilmercorrea97 <wilmercorrea97@gmail.com>
2023-03-26 14:28:39 -0300

    v1

---
+++ suppliers-service/src/main/java/com/example/demo/services/ISupplierService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Supplier;
+
+import java.util.List;
+
+public interface ISupplierService {
+    List<Supplier> getAll();
+
+    Supplier getById(Long id);
+    void remove(Long id);
+    void save(Supplier supplier);
+}
